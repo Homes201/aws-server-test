673ee58122c394bd1cb4d61e7293d2664048c07e
sangjip <tkdwlqdl12@gmail.com>
2024-12-04 16:06:19 +0900

    wrong docker

--- aws_test/urls.py
+++ aws_test/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for btr_search project.
+URL configuration for aws-server-test project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
