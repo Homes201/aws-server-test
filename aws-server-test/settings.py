25354b4854fb7214f675b94d4f4192aa985ea104
sangjip <tkdwlqdl12@gmail.com>
2024-12-05 12:40:05 +0900

    wrong settings

--- aws-server-test/settings.py
+++ aws-server-test/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         "ENGINE": os.environ.get("POSTGRES_ENGINE", "django.db.backends.postgresql"),
         "NAME": os.environ.get("POSTGRES_DB_NAME" , "postgres"),
         "USER" : os.environ.get("POSTGRES_USER", "user"),
-        "PASSWORD" : os.environ.get("POSTGRES_PASSWORD", "testtest123!"),
+        "PASSWORD" : os.environ.get("POSTGRES_PASSWORD", "password"),
         "HOST" : os.environ.get("POSTGRES_HOST", "localhost"),
         "PORT" : os.environ.get("POSTGRES_PORT", "5432"),
     }
